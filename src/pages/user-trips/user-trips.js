2a3dc68a94d09e8cf90b654fcd0fe6418fa7fedb
michael ayad <micheltwfik@gmail.com>
2022-12-25 07:09:57 +0200

    finish trips page

--- src/pages/user-trips/user-trips.js
+++ src/pages/user-trips/user-trips.js
@@ -11,8 +11,9 @@ import TripCart from "../../components/trips/trip-card";
 
 const UserTrips = () => {
     const { t, i18n } = useTranslation();
-    const [isLoading, setLoading] = useState(false);
+    const [isLoading, setLoading] = useState(true);
     const [buttonsRadio, setButtonRadio] = useState(0);
+    const [trips, setTrips] = useState([]);
     const history = useHistory();
     const user = useSelector((state) => state.user);
     const token = localStorage.getItem("token");
@@ -27,17 +28,17 @@ const UserTrips = () => {
         axiosInstance.get(`/reservations?lang=${i18n.language}`, config).then((res) => {
             console.log(res.data);
             // reservation-successful
-
+            setTrips(res.data);
+            setLoading(false);
         })
             .catch((err) => {
                 console.log(err);
                 console.log("user", user._id);
                 console.log("token", token);
             });
-        console.log("user", user._id);
-        console.log("token", token);
 
-    }, [user]);
+
+    }, [i18n.language]);
 
 
     const handleSearch = () => {
@@ -60,37 +61,35 @@ const UserTrips = () => {
                 {/* ---------- component header ---------- */}
                 <h2 className="my-3 fw-bold">{t("Trips")}</h2>
                 <Divider style={{ background: "#757575" }} className="my-3" />
+                {trips.length ? <div className="my-4">
+                    {/* <div className="btn-group mb-4" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" onClick={() => { handleRadio(0) }} checked={buttonsRadio === 0} disabled={buttonsRadio === 0} />
+                        <label className="btn btn-outline-dark" htmlFor="btnradio1">All Trips</label>
 
-                <div className="my-4">
+                        <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off" onClick={() => { handleRadio(1) }} checked={buttonsRadio === 1} disabled={buttonsRadio === 1} />
+                        <label className="btn btn-outline-dark" htmlFor="btnradio2">Past Trips</label>
+
+                        <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autocomplete="off" onClick={() => { handleRadio(2) }} checked={buttonsRadio === 2} disabled={buttonsRadio === 2} />
+                        <label className="btn btn-outline-dark" htmlFor="btnradio3">Present Trips</label>
+
+                        <input type="radio" className="btn-check" name="btnradio" id="btnradio4" autocomplete="off" onClick={() => { handleRadio(3) }} checked={buttonsRadio === 3} disabled={buttonsRadio === 3} />
+                        <label className="btn btn-outline-dark" htmlFor="btnradio4">upcomming Trips</label>
+                    </div> */}
+                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3" >
+
+                        {trips.map((trip, index) => {
+                            return (<div className="col p-3" key={index}><div className="col-12 p-3 rounded-3" style={{ border: "1px solid #6f7174" }}><TripCart trip={trip} /></div></div>
+                            );
+                        })}</div>
+
+                    {/* ----------------------------------------------------- */}
+                </div> : <div className="my-4">
                     <h4 className="">{t("No trips booked...yet!")}</h4>
                     <p className="text-secondary">{t("Time to dust off your bags and start planning your next adventure")}</p>
                     <button type="button" class="btn btn-outline-dark px-4 py-3 my-2" onClick={handleSearch}>{t("Start searching")}</button>
-                </div>
-                <div className="my-4">
-                    <div className="btn-group mb-4" role="group" aria-label="Basic radio toggle button group">
-                        <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" onClick={() => { handleRadio(0) }} checked={buttonsRadio === 0} disabled={buttonsRadio === 0} />
-                        <label className="btn btn-outline-dark" for="btnradio1">All Trips</label>
-
-                        <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off" onClick={() => { handleRadio(1) }} checked={buttonsRadio === 1} disabled={buttonsRadio === 1} />
-                        <label className="btn btn-outline-dark" for="btnradio2">Past Trips</label>
-
-                        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" onClick={() => { handleRadio(2) }} checked={buttonsRadio === 2} disabled={buttonsRadio === 2} />
-                        <label className="btn btn-outline-dark" for="btnradio3">Present Trips</label>
-
-                        <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off" onClick={() => { handleRadio(3) }} checked={buttonsRadio === 3} disabled={buttonsRadio === 3} />
-                        <label className="btn btn-outline-dark" for="btnradio4">upcomming Trips</label>
-                    </div>
-                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                        <div className="p-3"><div className="col-12 p-3 rounded-3" style={{ border: "1px solid #6f7174" }}><TripCart /></div></div>
-                        <div className="p-3"><div className="col-12 p-3 rounded-3" style={{ border: "1px solid #6f7174" }}><TripCart /></div></div>
-                        <div className="p-3"><div className="col-12 p-3 rounded-3" style={{ border: "1px solid #6f7174" }}><TripCart /></div></div>
-                        <div className="p-3"><div className="col-12 p-3 rounded-3" style={{ border: "1px solid #6f7174" }}><TripCart /></div></div>
-                        <div className="p-3"><div className="col-12 p-3 rounded-3" style={{ border: "1px solid #6f7174" }}><TripCart /></div></div>
+                </div>}
 
 
-
-                    </div>
-                </div>
                 <Divider style={{ background: "#757575" }} className="my-3" />
                 <p style={{ fontSize: "14px" }} className="my-3">
                     {t("Can’t find your reservation here?")}{" "}
